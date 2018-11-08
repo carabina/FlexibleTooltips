3b9dfc6c52dfcfcb943fd8effda5789efe036546
Chen-Hao Chiang <chenhao.chiang@gmail.com>
2018-11-08 17:22:59 +0100

    changed file positions and corrected some errors

---
+++ Example/Pods/Target Support Files/FlexibleTooltips/FlexibleTooltips-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FlexibleTooltips : NSObject
+@end
+@implementation PodsDummy_FlexibleTooltips
+@end
---
+++ Example/Pods/Target Support Files/Pods-FlexibleTooltips_Example/Pods-FlexibleTooltips_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlexibleTooltips_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FlexibleTooltips_Example
+@end
